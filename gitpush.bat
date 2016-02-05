65f9b9d100cf5e06d3f96ca2659a1409b1ba9451
unknown <KH9126@KH9126.kitspl.com>
2016-02-05 11:19:00 +0530

    Test finished

--- gitpush.bat
+++ gitpush.bat
@@ -1,8 +1,21 @@
 @echo off
+setlocal enableDelayedExpansion
 IF [%1] == [] GOTO Blank
-SET "first=%1"
-abc init
-xyz aa
+SET "username=%1"
+SET "github=https://github.com/"
+SET "project=mrndmocktest_feb21.git"
+SET "slash=/"
+SET "originpath=%github%%username%%slash%%project%"
+echo Make sure your Github Repo url is this
+echo !originpath!
+
+git init
+git remote add origin %originpath%
+git pull origin master
+git add .
+git commit -m "Test finished"
+git push origin master
+
 GOTO End
 
 
